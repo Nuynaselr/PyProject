dfc7e67888bcf2819c992cab8b4b559653e0c727
Nuynaselr <nikita_parenskii@mail.ru>
2019-08-24 17:07:32 +0400

    __

--- work/processingDB.py
+++ work/processingDB.py
@@ -1,5 +1,26 @@
 import mysql.connector
 from configparser import ConfigParser
+#DELETE from monitoring_system where PID = 747;
+
+data_base = {}
+
+
+def gen_element(UID, PID, PPID, C, SZ, RSS, PSR, STIME, TTY, TIME, CMD):
+    dicti = {
+        "UID": UID,
+        "PID": PID,
+        "PPID": PPID,
+        "C": C,
+        "SZ": SZ,
+        "RSS": RSS,
+        "PSR": PSR,
+        "STIME": str(STIME),
+        "TTY": TTY,
+        "TIME": TIME,
+        "CMD": CMD
+    }
+    return dicti
+
 
 def read_db_config(filename='config.ini', section='mysql'):
     # create parser and read ini configuration file
@@ -17,27 +38,18 @@ def read_db_config(filename='config.ini', section='mysql'):
 
     return db
 
-if __name__ == '__main__':
-    try:
-        data_base = read_db_config()
 
+def delete_by_pid(PID):
+    try:
         connect = mysql.connector.connect(host=data_base.get('host'),
                                           database=data_base.get('database'),
                                           user=data_base.get('user'),
                                           password=data_base.get('password'))
 
         cursor = connect.cursor()
-        cursor.execute('SELECT * FROM monitoring_system WHERE UID != "root"')
+        cursor.execute('DELETE FROM monitoring_system WHERE PID = %s', (PID,))
 
-        array_pid = []
-        row = cursor.fetchone()
-
-        while row is not None:
-            print(row)
-            array_pid.append(row[1])
-            row = cursor.fetchone()
-
-        print(array_pid)
+        connect.commit()
 
     except mysql.connector.Error as e:
         print(e)
@@ -45,3 +57,37 @@ if __name__ == '__main__':
     finally:
         cursor.close()
         connect.close()
+
+
+def read_db():
+    json_db = []
+
+    try:
+        dbconfig = read_db_config()
+        connect = mysql.connector.connect(host=data_base.get('host'),
+                                          database=data_base.get('database'),
+                                          user=data_base.get('user'),
+                                          password=data_base.get('password'))
+        cursor = connect.cursor()
+        cursor.execute("SELECT * FROM books")
+
+        row = cursor.fetchone()
+
+        while row is not None:
+            print(row)
+            json_db.append(gen_element(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10]))
+            row = cursor.fetchone()
+
+    except mysql.connector.Error as e:
+        print(e)
+
+    finally:
+        cursor.close()
+        connect.close()
+
+    return json_db
+
+
+if __name__ == '__main__':
+    data_base = read_db_config()
+
