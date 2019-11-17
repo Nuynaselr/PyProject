9cb45b0b9cf643493c90476e1dad4ba58835fdb0
Nuynaselr <nikita_parenskii@mail.ru>
2019-11-18 00:49:52 +0400

    add: [add average congestion CPU]

--- work/statistics/GetStatistics.py
+++ work/statistics/GetStatistics.py
@@ -1,9 +1,13 @@
 import mysql.connector
 from configparser import ConfigParser
 from os import path
+import copy
 
 data_base = {}
-
+tuple_months = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+                'October', 'November', 'December')
+exclusive_tables = ['2019_10_12_23_03', '', '']
+# exclusive_tables = ['2019_10_09_18_50', '2019_10_10_15_57', '2019_10_16_20_51', '2019_11_01_12_00']
 
 def get_list_table():
     try:
@@ -67,7 +71,7 @@ def get_list_user(name_table):
         connect.close()
 
 
-def get_data_from_table(name_table):
+def get_data_from_table_CPU(name_table):
     try:
         connect = mysql.connector.connect(host=data_base.get('host'),
                                           database=data_base.get('database'),
@@ -75,12 +79,15 @@ def get_data_from_table(name_table):
                                           password=data_base.get('password'))
 
         cursor = connect.cursor()
-        read_row = 'SELECT USER, CPU, MEM, GPU, GMEM, ENDTIME FROM ' + name_table
+        read_json = {}
+        read_row = 'SELECT USER, AVG(CPU), AVG(MEM), SUM(ENDTIME - STIME) FROM ' + name_table + ' GROUP BY USER'
 
         cursor.execute(read_row)
         data_from_db = cursor.fetchall()
+        for element in data_from_db:
+            read_json[element[0]] = [element[1], element[2], element[3]]
 
-        return data_from_db
+        return read_json
 
     except mysql.connector.Error as error:
         print(error)
@@ -90,14 +97,32 @@ def get_data_from_table(name_table):
         connect.close()
 
 
-def check_list_user(new_user_list, user_list, main_json):
-    for element in new_user_list:
-        if element not in user_list:
-            user_list.append(element)
+def convert_to_structure_per_month_CPU(list_table):
+    # list_user = get_list_user(table)
+    data_from_table = []
+    for table in list_table:
+        data_from_table.append(get_data_from_table_CPU(table))
 
+    final_dict_data = copy.deepcopy(data_from_table[0])
 
-def convert_to_structure(user_list, data_from_table):
-    pass
+    for element in data_from_table:
+        for element_dictionary in element.keys():
+            if element_dictionary in final_dict_data:
+                final_dict_data.get(element_dictionary)[0] += element.get(element_dictionary)[0]
+                final_dict_data.get(element_dictionary)[1] += element.get(element_dictionary)[1]
+                final_dict_data.get(element_dictionary)[2] += element.get(element_dictionary)[2]
+            else:
+                final_dict_data[element_dictionary] = [element.get(element_dictionary)[0],
+                                                       element.get(element_dictionary)[1],
+                                                       element.get(element_dictionary)[2]]
+
+    length_for_avg = len(data_from_table)
+    for element in final_dict_data:
+        final_dict_data.get(element)[0] /= length_for_avg
+        final_dict_data.get(element)[1] /= length_for_avg
+
+    for element in final_dict_data:
+        print('{0}: {1}'.format(element, final_dict_data.get(element)))
 
 
 def read_db_config(filename, section='mysql'):
@@ -117,9 +142,27 @@ def read_db_config(filename, section='mysql'):
     return db
 
 
+def create_true_list_table(list_tables):
+    true_list_tables = {}
+    for element in list_tables:
+        split_row = [str(x) for x in element.split('_')]
+        if split_row[1] in true_list_tables and element not in exclusive_tables:
+            true_list_tables.get(split_row[1]).append(element)
+        elif element not in exclusive_tables:
+            true_list_tables[split_row[1]] = [element]
+    return true_list_tables
+
+
 if __name__ == '__main__':
     path_to_config = path.dirname(path.abspath('')) + '/config.ini'
     print(path_to_config)
     data_base = read_db_config(path_to_config)
     table_from_db = get_list_table()
-    print(get_data_from_table(table_from_db[0]))
+
+    table_from_db = create_true_list_table(table_from_db)
+    sorted(table_from_db)
+    for element_arr_table in table_from_db:
+        print(element_arr_table)
+        convert_to_structure_per_month_CPU(table_from_db.get(element_arr_table))
+
+    # convert_to_structure_per_month(table_from_db)
